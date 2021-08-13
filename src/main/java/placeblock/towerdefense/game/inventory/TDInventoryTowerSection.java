441604b9b5cdacc3968cebbf820f5b93419a6cb1
PlaceblockHD <felix.weglehner@gmx.de>
2021-08-13 09:03:53 +0200

    Added ProtocolLib for Tower Interaction Added InventorySections to handle hotbar of Player Added Custom Tower Skins (can be set in config) Fixed some bugs

--- src/main/java/placeblock/towerdefense/game/inventory/TDInventoryTowerSection.java
+++ src/main/java/placeblock/towerdefense/game/inventory/TDInventoryTowerSection.java
@@ -1,8 +1,12 @@
 package placeblock.towerdefense.game.inventory;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import placeblock.towerdefense.TowerDefense;
 import placeblock.towerdefense.game.TDPlayer;
 import placeblock.towerdefense.game.TDTower;
 
@@ -27,7 +31,18 @@ public class TDInventoryTowerSection extends TDInventorySection{
 
     @Override
     public void interact(PlayerInteractEvent e) {
-
+        if(e.getItem() == null) return;
+        ItemStack item = e.getItem();
+        if(!item.getItemMeta().getPersistentDataContainer().has(new NamespacedKey(TowerDefense.getInstance(), "data"), PersistentDataType.STRING)) return;
+        String data = item.getItemMeta().getPersistentDataContainer().get(new NamespacedKey(TowerDefense.getInstance(), "data"), PersistentDataType.STRING);
+        switch(data.toLowerCase()) {
+            case "sell":
+                break;
+            case "upgrade":
+                break;
+            case "info":
+                break;
+        }
     }
 
     @Override
